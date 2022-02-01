09b8bd2e60001a56c708edc4db4dce1078e4853c
kristo <56217351+KristoTammeleht@users.noreply.github.com>
2022-02-01 13:48:17 +0200

    Update Harjutus01.py

--- Harjutus01.py
+++ Harjutus01.py
@@ -2,7 +2,7 @@
 #kristo tammeleht IT-21
 #31.01.22
 
-'''
+
 print ("tere maailm") #sõne
 print("tere sa ka")#string
 print(2000)#int
@@ -10,34 +10,31 @@ print(2.5)#float
 print('Ütle"tere maailm"')
 print("Ütle\"tere maailm\"")#sec string
 print(type(2000))#näitab andme tüüpi
-'''
-'''
+
+
 a = 12
 b = 23
 s = a*b
 p = 2*(a*b)
 print ("pindala",s,"ruutmeetrit")
 print ("ümbermööt",p,"meetrit")
-'''
-'''
+
+
 nimi = input("lisa oma nimi:")#pluss ei lisa tühikut aga koma lisab
 vanus = int(input("vanus:"))
 print("tere",nimi+"!")
 vanuste_vahe = 17-vanus
 print ("Meie vanuste vahe on:",vanuste_vahe,"a")
-'''
-'''
-print ("\"Cabernet Sauvignon\" või 'Cabernet Sauvignon'?")
 
+
+print ("\"Cabernet Sauvignon\" või 'Cabernet Sauvignon'?")
 print              ("  @..@")
 print (" (----)")
 print                         ("( \__/ )")
 print   ('^^ "" ^^')
-'''
 
 
 a= int(input("Sisesta ruudu külje pikkus:"))
 p = a * a
 print("Vastus: ruudu pindala on",p,"cm^2")
-
 
