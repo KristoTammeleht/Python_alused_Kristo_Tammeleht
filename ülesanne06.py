a46dbff93cc3c151fc2f9d294d7350fd13bb27b9
kristo <56217351+KristoTammeleht@users.noreply.github.com>
2022-02-15 11:45:10 +0200

    Add files via upload

---
+++ ülesanne06.py
@@ -0,0 +1,24 @@
+
+era = []
+
+with open('S6.txt','r') as fail:   
+    for i in fail:
+        rida = i.split(" ")
+        
+        print(f"{rida[0]:11} {rida[1]:11} {rida[2]:11} {rida[3]:11}")
+        if rida[2] not in era:
+            era.append(rida[2])
+
+with open("S6.txt") as f:
+    contents = f.read()
+    count = contents.count("RE")
+    count2 = contents.count("KE")
+    print ('Reformikaid:',count)
+    print ('Kesikud:',count2)
+
+
+        
+er = set(era)
+kokku = len(er)
+print(kokku)
+
